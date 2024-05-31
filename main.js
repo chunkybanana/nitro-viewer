30985e57c34a01332da55525d04c7701ee70b9e7
Chunkybanana <chunkybananagithub@gmail.com>
2024-06-01 08:37:25 +1200

    p4c5

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const ppPart = document.getElementById("ppPart"), pPart = document.getElementByI
 const ppChapter = document.getElementById("ppChapter"), pChapter = document.getElementById("pChapter"), nChapter = document.getElementById("nChapter"), nnChapter = document.getElementById("nnChapter");
 const ppComic = document.getElementById("ppComic"), pComic = document.getElementById("pComic"), nComic = document.getElementById("nComic"), nnComic = document.getElementById("nnComic");
 
-function loadComic(){
+function loadComic() {
     location.hash = `${partNum}-${chapter}-${comic}`;
     let comicImg = document.getElementById('comicimg');
     comicImg.src = `comics/p${partNum}/c${chapter}/${comic}.png`;
@@ -12,7 +12,7 @@ function loadComic(){
     document.getElementById('part').innerText = partNum + " / " + parts.length;
     document.getElementById('chapter').innerText = chapter + " / " + parts[partNum - 1].length;
     document.getElementById('comicnum').innerText = comic + " / " + parts[partNum - 1][chapter - 1].length;
-    
+
     preloadImage(partNum, chapter, comic + 1);
     preloadImage(partNum, chapter, comic + 2);
     preloadImage(partNum, chapter, comic + 3);
@@ -27,22 +27,22 @@ function loadComic(){
 
     preloadImage(1, 1, 1);
     preloadImage(2, 1, 1);
-    
+
     pPart.disabled = partNum == 1;
     nPart.disabled = partNum == parts.length;
     pChapter.disabled = chapter == 1 && partNum == 1;
     nChapter.disabled = chapter == parts[partNum - 1].length && partNum == parts.length;
     pComic.disabled = comic == 1 && chapter == 1 && partNum == 1;
     nComic.disabled = comic == parts[partNum - 1][chapter - 1].length && chapter == parts[partNum - 1].length && partNum == parts.length;
-    
+
     let titleText = document.getElementById("title");
-    
-    titleText.textContent = parts[partNum - 1][chapter - 1][comic - 1];
+
+    titleText.innerText = parts[partNum - 1][chapter - 1][comic - 1];
 }
-function preloadImage(part, chapter, comic){
-    if(part <= 0 || part > parts.length || chapter <= 0 || chapter > parts[part - 1].length || comic <= 0 || comic > parts[part - 1]?.[chapter - 1]?.length) return;
+function preloadImage(part, chapter, comic) {
+    if (part <= 0 || part > parts.length || chapter <= 0 || chapter > parts[part - 1].length || comic <= 0 || comic > parts[part - 1]?.[chapter - 1]?.length) return;
     let url = `comics/p${part}/c${chapter}/${comic}.png`;
-    if(!images.find(value => value.src == 'https://se-nitro.surge.sh/' + url)){
+    if (!images.find(value => value.src == 'https://se-nitro.surge.sh/' + url)) {
         let img = new Image();
         img.src = url;
         images.push(img)
@@ -60,113 +60,113 @@ ppPart.addEventListener("click", () => {
     comic = 1;
     chapter = 1;
     partNum = 1;
-    
+
     loadComic();
 }, false);
 pPart.addEventListener("click", () => {
     comic = 1;
     chapter = 1;
     partNum -= 1;
-    
+
     loadComic();
 }, false);
 nPart.addEventListener("click", () => {
     comic = 1;
     chapter = 1;
     partNum += 1;
-    
+
     loadComic();
 }, false);
 nnPart.addEventListener("click", () => {
     comic = 1;
     chapter = 1;
     partNum = parts.length;
-    
+
     loadComic();
 }, false);
 
 ppChapter.addEventListener("click", () => {
     comic = 1;
     chapter = 1;
-    
+
     loadComic();
 }, false);
 pChapter.addEventListener("click", () => {
     comic = 1;
     chapter -= 1;
-    
+
     if (chapter == 0) {
         partNum -= 1;
         chapter = parts[partNum - 1].length;
     }
-    
+
     loadComic();
 }, false);
 nChapter.addEventListener("click", () => {
     comic = 1;
     chapter += 1;
-    
+
     if (chapter > parts[partNum - 1].length) {
         partNum += 1;
         chapter = 1;
     }
-    
+
     loadComic();
 }, false);
 nnChapter.addEventListener("click", () => {
     comic = 1;
     chapter = parts[partNum - 1].length;
-    
+
     loadComic();
 }, false);
 
 ppComic.addEventListener("click", () => {
     comic = 1;
-    
+
     loadComic();
 }, false);
 pComic.addEventListener("click", () => {
     comic -= 1;
-    
+
     if (comic == 0) {
         chapter -= 1;
-        
+
         if (chapter == 0) {
             partNum -= 1;
             chapter = parts[partNum - 1].length;
         }
-        
+
         comic = parts[partNum - 1][chapter - 1].length;
     }
-    
+
     loadComic();
 }, false);
 nComic.addEventListener("click", () => {
     comic += 1;
-    
+
     if (comic > parts[partNum - 1][chapter - 1].length) {
         chapter += 1;
-        
+
         if (chapter > parts[partNum - 1].length) {
             partNum += 1;
             chapter = 1;
         }
-        
+
         comic = 1;
     }
-    
+
     loadComic();
 }, false);
 nnComic.addEventListener("click", () => {
     comic = parts[partNum - 1][chapter - 1].length;
-    
+
     loadComic();
 }, false);
 
 window.addEventListener("keydown", (info) => {
     if (info.ctrlKey || info.altKey || info.metaKey || info.shiftKey)
         return;
-    
+
     if (info.code == "ArrowLeft" || info.code == "KeyA" || info.code == "KeyH")
         pComic.click();
     else if (info.code == "ArrowRight" || info.code == "KeyD" || info.code == "KeyL")
